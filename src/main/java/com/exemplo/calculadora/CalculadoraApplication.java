9d352d0b2cdb77a0172e8e680e8782ae35028351
Fabrício Carvalho <98437353+FabricioCarvalho-Dev@users.noreply.github.com>
2025-09-22 12:02:14 -0300

    Update CalculadoraApplication.java

--- src/main/java/com/exemplo/calculadora/CalculadoraApplication.java
+++ src/main/java/com/exemplo/calculadora/CalculadoraApplication.java
@@ -3,6 +3,7 @@ package com.exemplo.calculadora;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class CalculadoraApplication {
     public static void main(String[] args) {
